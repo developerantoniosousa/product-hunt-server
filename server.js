9b3854321b966831e55aa4d9518bfd753af03e78
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-04 11:29:23 -0300

    feat: connection with mongodb

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/product-hunt", {
+  useNewUrlParser: true,
+});
 
 app.listen(3001);
