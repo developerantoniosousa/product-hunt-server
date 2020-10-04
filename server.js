9dc56325a5c51359874c27dc24a6f82294940dd8
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-04 12:19:58 -0300

    feat: set up cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors("*"));
 
 mongoose.connect("mongodb://localhost:27017/product-hunt", {
   useNewUrlParser: true,
