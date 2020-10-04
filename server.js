317c7e74ed139578607b1e28dbcc031fd5102049
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-04 11:53:38 -0300

    feat: turn on capacity to work with json struture

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/product-hunt", {
   useNewUrlParser: true,
