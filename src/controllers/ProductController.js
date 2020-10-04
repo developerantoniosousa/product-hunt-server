0e9f3bec45489bdf854ce071e770cb0efc4e39c2
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-04 11:46:40 -0300

    feat: listing of products

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const Product = mongoose.model("Product");
+
+module.exports = {
+  async index(request, response) {
+    const products = await Product.find();
+    return response.json(products);
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+const ProductController = require("./controllers/ProductController");
+
 const routes = express.Router();
+
+routes.get("/products", ProductController.index);
 
 module.exports = routes;
